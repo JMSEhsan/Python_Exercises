a3feb64fb6176b19e18cb4430c2aedd2f0c4006e
Ehsan Jalali <JMSEhsan@gmail.com>
2021-10-27 23:47:32 -0600

    initialize a practice

---
+++ PracticePython/P1_SwapValues.py
@@ -0,0 +1,13 @@
+# Ref.: Head First Learn To Code
+# Swap the values
+
+first = 'somewhere'
+last = 'over the rainbow'
+
+print(first, last)
+
+temp = first
+first = last
+last = temp
+
+print(first, last)
