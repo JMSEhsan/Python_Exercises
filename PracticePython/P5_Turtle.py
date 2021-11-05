9cc103f5f2edf889ae5128b2909cd2167c8c1a38
Ehsan Jalali <JMSEhsan@gmail.com>
2021-11-04 22:14:43 -0600

    developed

---
+++ PracticePython/P5_Turtle.py
@@ -0,0 +1,24 @@
+# Ref.: Head First Learn To Code
+# Turtle
+
+import turtle
+slowpoke = turtle.Turtle()
+slowpoke.fillcolor('green')
+slowpoke.shape('turtle')
+
+slowpoke.forward(300)
+slowpoke.left(90)
+slowpoke.forward(300)
+slowpoke.left(90)
+slowpoke.forward(600)
+slowpoke.left(90)
+slowpoke.forward(600)
+slowpoke.left(90)
+slowpoke.forward(600)
+slowpoke.left(90)
+slowpoke.forward(300)
+slowpoke.left(90)
+slowpoke.forward(300)
+slowpoke.right(7*360)
+
+turtle.mainloop()
