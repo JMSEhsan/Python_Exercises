6031303fe7016957918c577d8071ed97163958f7
Ehsan Jalali <JMSEhsan@gmail.com>
2021-11-08 23:01:29 -0700

    Initialize P_6; developed same

---
+++ PracticePython/P_6_UI.py
@@ -0,0 +1,9 @@
+# Ref. Get Programming Learn to code with Python by Ana Bell
+# UI: Creating a Window
+import tkinter
+
+window = tkinter.Tk()
+window.geometry("800x200")
+window.title("My first GUI")
+window.configure(background = "#000080")
+window.mainloop()
