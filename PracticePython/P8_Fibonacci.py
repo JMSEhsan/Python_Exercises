3de2de532a68e6e5ef502c8d132f23da33e97b05
Ehsan Jalali <JMSEhsan@gmail.com>
2021-11-10 18:38:01 -0700

    modified P7, added a loop

--- PracticePython/P8_Fibonacci.py
+++ PracticePython/P8_Fibonacci.py
@@ -1,15 +1,20 @@
 # Ref.: Classic Computer Science Problems in Python by David Kopec, Manning, 2019
-# recursive attempt
+# Fibonacci recursive attempt 
 # I added to the orignal code
 
 def fib(n: int) -> int:
     if n >=0 and n < 2:         # base case
         return n
     elif n < 0:
-        print("A negative number is not in Fibonatcci series!")
+        print("A negative number is not in Fibonacci sequence!")
     else:
         return fib(n - 2) + fib(n - 1) # recursive case
-
-x = int(input("Enter an integer: "))
-if __name__ == "__main__":
-    print("fib(", x ,") -> ", fib(x))
+e = True
+while e == True:
+    try:
+        x = int(input("Enter an integer, (Press any letter to exit): "))
+        if __name__ == "__main__":
+            print("fib(", x+1 ,") -> ", fib(x))
+    except ValueError:
+        print("Thank you!")
+        e = False
